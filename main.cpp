e4f2d902d85b02b7f8816efcdbda4398a2d7eaf8
SaZix <sazix@MacBook-Air-SaZix.local>
2025-09-16 20:38:18 +0300

    Some changes frome main branch

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@ int main(int argc, char** argv)
 {
 	if(argc >= 2) std::cout << "Hello, " << argv[1] << std::endl;
 	else std::cout << "Hello, world!\n";
+
+	std::cout << "This is the main brach" << std::endl;
 	
 	return 0;
 }
